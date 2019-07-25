c8fb7bcf8eb741680cb8b620ac9d10564180cc63
daviwang <daviwang@umich.edu>
2019-07-24 22:13:17 -0400

    Created buttons and button listeners for 'true' and 'false' as well as toasts on click for each. This marks the completion of Chapter 1.

--- app/src/main/java/com/davidwang/geoquiz/MainActivity.java
+++ app/src/main/java/com/davidwang/geoquiz/MainActivity.java
@@ -2,12 +2,35 @@ package com.davidwang.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
